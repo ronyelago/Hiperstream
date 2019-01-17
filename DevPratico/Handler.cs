cf96d0447bde37fb3740e96f255e2d10fff65997
ronye <ronye.rocha@gmail.com>
2019-01-17 09:09:56 -0800

    almost done

---
+++ DevPratico/Handler.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DevPratico
+{
+    public static class Handler
+    {
+
+
+        //Este metodo espera um path de arquivo que sera 
+        //convertido para um DataTable e retornado
+        public static DataTable FileToDataTable(string path)
+        {
+            DataTable dataTable = new DataTable();
+            var rawFile = File.ReadAllLines(path);
+
+            if (rawFile.Length > 0)
+            {
+                //linha que representara os nomes das colunas
+                string firstLine = rawFile[0];
+                //cria o array com todos os nomes das colunas
+                string[] headerLabels = firstLine.Split(";");
+
+                //adiciona as colunas ao DataTable
+                foreach (var header in headerLabels)
+                {
+                    dataTable.Columns.Add(new DataColumn(header));
+                }
+
+                //inserindo dados no DataTable
+                for (int i = 1; i < rawFile.Length; i++)
+                {
+                    //cria um array com os dados de cada linha
+                    //cada item do array corresponde a um dado de coluna
+                    string[] line = rawFile[i].Split(";");
+
+                    //cria uma linha que sera adicionada ao DataTable
+                    DataRow dataRow = dataTable.NewRow();
+
+                    //preenche a nova linha com os dados do array
+                    for (int j = 0; j < dataRow.ItemArray.Length; j++)
+                    {
+                        dataRow[j] = line[j];
+                    }
+
+                    //adiciona a nova linha ao DataTable
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+
+            return dataTable;
+        }
+
+        //Validacao de cep 
+        //Regras => 8 digitos, numeros nao podem ser todos repetidos
+        //e nao pode ser null nem vazio
+        public static bool CepValidate(string cep)
+        {
+            cep = cep.Trim();
+
+            if (string.IsNullOrEmpty(cep) || cep.Length > 8 || Regex.IsMatch(cep, (@"^(\d)\1*$")))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DevPratico/Program.cs
@@ -0,0 +1,133 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+
+namespace DevPratico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "\\Baseficticia.txt");
+
+            var dir = Directory.GetCurrentDirectory();
+
+
+            var dataTable = Handler.FileToDataTable(path);
+
+            //-Remova todos os registros com CEP Inválido(aplicar regras);
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                var dr = dataTable.Rows[i];
+
+                if (!Handler.CepValidate(dr["CEP"].ToString()))
+                {
+                    dr.Delete();
+                }
+            }
+
+            dataTable.AcceptChanges();
+
+            //- Separe os registros com faturas zeradas e grave em um arquivo (.csv) a parte;
+
+            StringBuilder zeroeds = new StringBuilder();
+
+            zeroeds.Append("NomeCliente;");
+            zeroeds.Append("EnderecoCompleto;");
+            zeroeds.Append("ValorFatura;");
+            zeroeds.Append("NumeroPaginas;");
+
+            zeroeds.AppendLine();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                if (dr[6].ToString() == "0")
+                {
+                    //
+                    zeroeds.Append(dr[0].ToString() + "; ");
+
+                    StringBuilder address = new StringBuilder();
+                    address.Append(dr[1].ToString());
+                    address.Append(dr[2].ToString() + " ");
+                    address.Append(dr[3].ToString() + " ");
+                    address.Append(dr[4].ToString() + " ");
+                    address.Append(dr[5].ToString() + "; ");
+
+                    zeroeds.Append(address);
+                    zeroeds.Append(dr[6].ToString() + "; ");
+                    zeroeds.Append(dr[7].ToString());
+
+                    zeroeds.AppendLine();
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("FaturasZeradas.csv"))
+            {
+                writer.Write(zeroeds.ToString());
+            }
+
+            //- Agrupe os arquivos de saída por números de páginas de faturas
+
+            //registros com ate 6 paginas
+
+            StringBuilder sixPages = new StringBuilder();
+
+            sixPages.Append("NomeCliente;");
+            sixPages.Append("EnderecoCompleto;");
+            sixPages.Append("ValorFatura;");
+            sixPages.Append("NumeroPaginas;");
+            sixPages.AppendLine();
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                var dr = dataTable.Rows[i];
+                var strNum = dr["NumeroPaginas"].ToString();
+                int pages = int.Parse(strNum.Trim());
+
+                if (pages <= 6)
+                {
+                    if (pages % 2 == 1)
+                    {
+                        pages += 1;
+                    }
+
+                    dr["NumeroPaginas"] = pages;
+
+
+                    sixPages.Append(dr[0].ToString() + "; ");
+
+                    StringBuilder endereco = new StringBuilder();
+                    endereco.Append(dr[1].ToString());
+                    endereco.Append(dr[2].ToString() + " ");
+                    endereco.Append(dr[3].ToString() + " ");
+                    endereco.Append(dr[4].ToString() + " ");
+                    endereco.Append(dr[5].ToString() + "; ");
+
+                    sixPages.Append(endereco);
+                    sixPages.Append(dr[6].ToString() + "; ");
+                    sixPages.Append(dr[7].ToString());
+                }
+
+                sixPages.AppendLine();
+            }
+
+            using (StreamWriter writer = new StreamWriter("AteSeisPaginas.csv"))
+            {
+                writer.Write(sixPages.ToString());
+            }
+
+            //registros com ate 12 paginas
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
